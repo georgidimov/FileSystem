6e2a5f9021a77ab38a876f37b89a6ca33dfafa5a
Georgi Dimov <gt_dimov@mail.bg>
2015-02-11 20:29:47 +0200

    Add some methods in Directory.
    Add methods addDirectory, addFiles and getFile.

--- FileSystem/directory.cpp
+++ FileSystem/directory.cpp
@@ -1,7 +1,58 @@
 #include "directory.h"
 
+Directory :: Directory(Value newName, size_t size) : File(newName, size){
+    ;
+}
+
 Directory :: Directory(Value newName, size_t position, size_t newSize, size_t newSizeInFS) :
     File(newName, position, newSize, newSizeInFS)
 {
 
+}
+
+Directory :: ~Directory(){
+    int filesCount = files.getSize();
+
+    while(filesCount){
+        delete files[0];
+        files.removeAt(0);
+
+        --filesCount;
+    }
+
+    int dirsCount = directies.getSize();
+
+    while(dirsCount){
+        delete directies[0];
+        directies.removeAt(0);
+        --dirsCount;
+    }
+}
+
+void Directory :: addFile(File * file){
+    files.addAt(0, file);
+}
+
+void Directory :: addDirectory(Directory * dir){
+    directies.addAt(0, dir);
+}
+
+File * Directory :: getFile(Value name){
+    for(List<File *> :: Iterator it = files.begin(); it; it++){
+        if((*it)->getName() == name){
+            return *it;
+        }
+    }
+
+    return NULL;
+}
+
+Directory * Directory :: getDirectory(Value name){
+    for(List<Directory *> :: Iterator it = files.begin(); it; it++){
+        if((*it)->getName() == name){
+            return *it;
+        }
+    }
+
+    return NULL;
 }
--- FileSystem/directory.h
+++ FileSystem/directory.h
@@ -6,6 +6,15 @@ class Directory : public File{
     List<File *> files;
     List<Directory *> directies;
 public:
+    Directory(Value newName, size_t size);
     Directory(Value newName, size_t position, size_t newSize, size_t newSizeInFS);
+
+    ~Directory();
+
+    void addFile(File * file);
+    void addDirectory(Directory * dir);
+
+    File * getFile(Value name);
+
 };
 
