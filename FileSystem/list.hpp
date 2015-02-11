b8272bbf796d43661ecdc8063ffe71064140eb78
Georgi Dimov <gt_dimov@mail.bg>
2015-02-11 19:17:40 +0200

    Add iterator in List.

--- FileSystem/list.hpp
+++ FileSystem/list.hpp
@@ -22,14 +22,32 @@ public:
 
     void addAt(int index, T newElement);
 
-public:
     T removeAt(int index);
     const T & getAt(int index) const;
 
 
 
-   size_t getSize() const;
-   bool isEmpty() const;
+    size_t getSize() const;
+    bool isEmpty() const;
+
+    class Iterator{
+        Node<T> * current;
+    public:
+        Iterator();
+        Iterator(Node<T> * newCurrent);
+
+        Iterator & operator++(int uselessVar);
+
+        Iterator & operator++();
+
+        T operator *();
+
+        bool operator ==(const Iterator & r) const;
+        operator bool() const;
+    };
+
+    Iterator begin() const;
+    Iterator end() const;
 };
 
 
@@ -216,4 +234,55 @@ bool List<T> :: isEmpty() const{
 template <class T>
 size_t List<T> :: getSize() const{
     return size;
+}
+
+template <class T>
+typename List<T> :: Iterator List<T> :: begin() const{
+    return Iterator(first);
+}
+
+template <class T>
+typename List<T> :: Iterator List<T> :: end() const{
+    return Iterator();
+}
+
+
+template <class T>
+List<T> :: Iterator :: Iterator(){
+    current = NULL;
+}
+
+template <class T>
+List<T> :: Iterator :: Iterator(Node<T> * newCurrent){
+    current = newCurrent;
+}
+
+template <class T>
+typename List<T> :: Iterator & List<T> :: Iterator :: operator ++(int uselessVar){
+    ++uselessVar;
+
+
+    current = current->getNext();
+
+    return *(this);
+}
+
+template <class T>
+typename List<T> :: Iterator & List<T> :: Iterator :: operator ++(){
+    ++(*this);
+}
+
+template <class T>
+T List<T> :: Iterator ::operator *(){
+    return current->getValue();
+}
+
+template <class T>
+bool List<T> :: Iterator :: operator ==(const List<T> :: Iterator & rh) const{
+    return current == rh.current;
+}
+
+template <class T>
+List<T> :: Iterator :: operator bool() const{
+    return current != NULL;
 }
