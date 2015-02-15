6e0ee11e2a0233fc583feda13e39a8935fad7bf9
Georgi Dimov <gt_dimov@mail.bg>
2015-02-15 17:04:48 +0200

    Add Class PriorityQueue.

--- FileSystem/priorityqueue.hpp
+++ FileSystem/priorityqueue.hpp
@@ -5,6 +5,12 @@ template <class T>
 class PriorityQueue : public Queue<T>{
 public:
     void enqueue(T newElement);
+    T dequeue();
+    const T & peek() const;
+    bool contains(const T & value) const;
+
+    typename List<T> :: Iterator begin() const;
+    typename List<T> :: Iterator end() const;
 };
 
 ///FIX ME!
@@ -24,4 +30,36 @@ void PriorityQueue<T> :: enqueue(T newElement){
     }
 
     Queue<T> :: enqueue(newElement);
+}
+
+
+template <class T>
+T PriorityQueue<T> :: dequeue(){
+    return Queue<T> :: removeAt(0);
+}
+
+template <class T>
+const T & PriorityQueue<T> :: peek() const{
+    return Queue<T> :: operator [](0);
+}
+
+template <class T>
+typename List<T> :: Iterator PriorityQueue<T> :: begin() const{
+    return typename List<T> :: Iterator(List<T> :: first);
+}
+
+template <class T>
+typename List<T> :: Iterator PriorityQueue<T> :: end() const{
+    return List<T> :: Iterator();
+}
+
+template <class T>
+bool PriorityQueue<T> :: contains(const T & element) const{
+    for(typename PriorityQueue<T> :: Iterator it = begin(); it; it++){
+        if(*it == element){
+            return true;
+        }
+    }
+
+    return false;
 }
